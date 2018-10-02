2c334d745800b502afd041519de9114abc18a59e
Jonathan Silva Viana <jonathantepes@hotmail.com>
2018-10-01 22:01:50 -0300

    página index ou home do projeto

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'header.php';
+require_once 'titles.php';
+require_once 'conexao.php';
+
+
+?>
+
+<body>
+
+
+<nav class="blue darken-3" role="navigation">
+    <div class="nav-wrapper container">
+
+        <a id="logo-container" href="#" class="brand-logo">
+         <?php echo $title_Home ;?>
+        </a>
+
+        <?php require_once "nav_menu_home.php" ;?>
+
+    </div>
+</nav>
+
+<!--fim da barra de navegação-->
+
+<!--começo do conteúdo-->
+
+<?php require_once "home_c.php" ;?>
+
+<!--final do conteúdo-->
+
+<?php require_once "footer.php" ;?>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+
+</body>
+</html>
