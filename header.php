f0a682219cfa56cdc235ae001de7729d97dbb884
Jonathan Silva Viana <jonathantepes@hotmail.com>
2018-10-01 22:00:18 -0300

    arquivo de header

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'titles.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title><?= $presentation ;?></title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css" type="text/css" rel="stylesheet"/>
+    <link href="css/geral.css" type="text/css" rel="stylesheet"/>
+</head>
