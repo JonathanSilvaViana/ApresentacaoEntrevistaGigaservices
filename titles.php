da2469183f368514f41ebc14123a53a7a6d53252
Jonathan Silva Viana <jonathantepes@hotmail.com>
2018-10-01 22:09:04 -0300

    classe herdável da aplicação

---
+++ titles.php
@@ -0,0 +1,9 @@
+<?php
+
+$presentation = "Apresentação";
+$title_Home = "Teste prático - Menu inicial";
+$title_Categories  = "Lista Categoria";
+$title_Details_Categories = "Detalhes categorias";
+$title_Products = "Lista produtos";
+$title_Details_Products = "Detalhes produtos";
+
