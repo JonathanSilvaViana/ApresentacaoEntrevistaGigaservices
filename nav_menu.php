06f2d7fbca63aab0c96f4de08c1d003e4e5599f0
Jonathan Silva Viana <jonathantepes@hotmail.com>
2018-10-01 22:03:09 -0300

    arquivo de navegação

---
+++ nav_menu.php
@@ -0,0 +1,9 @@
+<ul class="right hide-on-med-and-down">
+    <li><a href="index.php"><i class="material-icons large left">home</i></a></li>
+</ul>
+
+<ul id="nav-mobile" class="sidenav indigo accent-4">
+    <li><a class="white-text" href="index.php">Home<i class="material-icons large left white-text">home</i></a></li>
+</ul>
+
+<a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
